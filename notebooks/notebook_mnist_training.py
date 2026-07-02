550fbf89ea05f48805b3896d65d96a38ccf7ee8b
xrChen <rain940707@gmail.com>
2026-07-02 23:41:40 +0800

    feat: 新增 CNN 類別以實現卷積神經網路模型

--- notebooks/notebook_mnist_training.py
+++ notebooks/notebook_mnist_training.py
@@ -16,3 +16,25 @@ x = train_data.data[0] #讀取訓練集中的第一張圖片
 plt.imshow(x) #把圖片顯示出來
 
 # %%
+class CNN(torch.nn.Module):
+  def __init__(self):
+    super(CNN, self).__init__()
+    self.conv1 = nn.Conv2d(1,16,kernel_size=(3,3)) #第一卷積層
+    self.conv2 = nn.Conv2d(16,32,kernel_size=(3,3))
+    self.maxpool = nn.MaxPool2d(kernel_size=(2,2)) #最大池化層
+    self.lin1 = nn.Linear(800,128)
+    self.out = nn.Linear(128,10) #模型的最後一層為線性層，用來處理CNN扁平化後的輸出
+  def forward(self,x):
+    x = self.conv1(x)
+    x = nn.functional.relu(x) #選用ReLU為激活函數
+    x = self.maxpool(x)
+    x = self.conv2(x)
+    x = nn.functional.relu(x)
+    x = self.maxpool(x)
+    x = x.flatten(start_dim=1) #扁平化
+    x = self.lin1(x) 
+    x = nn.functional.relu(x)
+    x = self.out(x)
+    x = nn.functional.log_softmax(x,dim=1) #使用log_softmax( )，以機率的形式進行分類
+    return x
+# %%
